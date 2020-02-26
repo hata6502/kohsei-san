b9eae1cc2f61cfe7ac190647a08dd7a69dc79cfc
Hata <hato6502@gmail.com>
2020-02-27 00:38:35 +0900

    material-ui を導入して

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import AppBar from '@material-ui/core/AppBar';
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
-ReactDOM.render(<h1>校正さん18koksssddddssscscscsco</h1>, document.querySelector('.app'));
+ReactDOM.render(
+  <>
+    <CssBaseline />
+
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6">校正さん</Typography>
+      </Toolbar>
+    </AppBar>
+
+    <Container>
+      <Paper>
+        <Container>
+          <TextField fullWidth label="タイトル" margin="normal" />
+          <TextField fullWidth margin="normal" multiline variant="outlined" />
+        </Container>
+      </Paper>
+    </Container>
+  </>,
+  document.querySelector('.app')
+);
