889acd63ea40f6338e1de90b20ebab173e3dfb94
Hata <hato6502@gmail.com>
2020-02-29 02:44:08 +0900

    textlint を導入して

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,30 @@ import ReactDOM from 'react-dom';
 import pink from '@material-ui/core/colors/pink';
 import purple from '@material-ui/core/colors/purple';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { createMuiTheme, StylesProvider, Theme, ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
+import {
+  createMuiTheme,
+  StylesProvider,
+  Theme,
+  ThemeProvider as MuiThemeProvider
+} from '@material-ui/core/styles';
 import App from './App';
+
+declare global {
+  interface Window {
+    kuromojin?: {
+      dicPath?: string;
+    };
+  }
+}
+
+window.kuromojin = {
+  dicPath: 'dict'
+};
 
 const theme = createMuiTheme({
   palette: {
     primary: pink,
-    secondary: purple,
+    secondary: purple
   }
 });
 
