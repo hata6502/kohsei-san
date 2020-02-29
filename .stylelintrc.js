895d19904a29f85574e81c74111c077a939cb4bc
Hata <hato6502@gmail.com>
2020-02-29 12:38:09 +0900

    stylelint を設定して

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,7 +1,11 @@
 module.exports = {
-  extends: ["stylelint-config-standard", "stylelint-prettier/recommended"],
+  extends: ["stylelint-config-standard", "stylelint-config-styled-components", "stylelint-prettier/recommended"],
   plugins: "stylelint-prettier",
+  processors: "stylelint-processor-styled-components",
   rules: {
-    "prettier/prettier": true
+    "prettier/prettier": true,
+    "value-keyword-case": ['lower', {
+      ignoreKeywords: ['dummyValue']
+    }]
   }
 };
