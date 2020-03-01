09cb4ca6263774fed755abfb93b5374f01420791
Hata <hato6502@gmail.com>
2020-03-01 23:54:15 +0900

    共有機能を作成して

--- src/Edit.tsx
+++ src/Edit.tsx
@@ -1,27 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import Alert, { AlertProps } from '@material-ui/lab/Alert';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import Paper from '@material-ui/core/Paper';
 import Snackbar from '@material-ui/core/Snackbar';
 import TextField from '@material-ui/core/TextField';
+import ShareIcon from '@material-ui/icons/Share';
 import * as Sentry from '@sentry/browser';
 import { TextlintMessage } from '@textlint/kernel';
 import lint from './lint';
+
+declare global {
+  interface Navigator {
+    share?: (data?: { text?: string, title?: string, url?: string }) => Promise<void>;
+  }
+}
 
 export interface EditProp {
   dispatchIsLinting: React.Dispatch<boolean>;
 }
 
 const Edit: React.FunctionComponent<EditProp> = ({ dispatchIsLinting }) => {
-  const [content, setContent] = useState(() => localStorage.getItem('content') || '');
   const [isLintErrorOpen, setIsLintErrorOpen] = useState(false);
   const [isSaveErrorOpen, setIsSaveErrorOpen] = useState(false);
   const [messages, setMessages] = useState<TextlintMessage[]>([]);
+  const [text, setText] = useState(() => localStorage.getItem('text') || '');
   const [title, setTitle] = useState(() => localStorage.getItem('title') || '');
 
   useEffect(() => {
     try {
-      localStorage.setItem('content', content);
+      localStorage.setItem('text', text);
       localStorage.setItem('title', title);
     } catch (exception) {
       setIsSaveErrorOpen(true);
@@ -30,10 +39,20 @@ const Edit: React.FunctionComponent<EditProp> = ({ dispatchIsLinting }) => {
       console.error(exception);
       Sentry.captureException(exception);
     }
-  }, [content, title]);
+  }, [text, title]);
 
-  const handleContentBlur: React.FocusEventHandler<HTMLTextAreaElement> = ({ target }) => {
-    setContent(target.value);
+  const handleLintErrorClose: AlertProps['onClose'] = () => setIsLintErrorOpen(false);
+
+  const handleSaveErrorClose: AlertProps['onClose'] = () => setIsSaveErrorOpen(false);
+  
+  const handleShareClick: React.MouseEventHandler = () => navigator.share?.({
+    text,
+    title,
+    url: 'https://kohsei-san.b-hood.site/',
+  });
+
+  const handleTextBlur: React.FocusEventHandler<HTMLTextAreaElement> = ({ target }) => {
+    setText(target.value);
 
     dispatchIsLinting(true);
     setTimeout(async () => {
@@ -53,10 +72,6 @@ const Edit: React.FunctionComponent<EditProp> = ({ dispatchIsLinting }) => {
     });
   };
 
-  const handleLintErrorClose: AlertProps['onClose'] = () => setIsLintErrorOpen(false);
-
-  const handleSaveErrorClose: AlertProps['onClose'] = () => setIsSaveErrorOpen(false);
-
   const handleTitleBlur: React.FocusEventHandler<HTMLTextAreaElement> = ({ target }) =>
     setTitle(target.value);
 
@@ -71,20 +86,30 @@ const Edit: React.FunctionComponent<EditProp> = ({ dispatchIsLinting }) => {
             margin="normal"
             onBlur={handleTitleBlur}
           />
+
           <TextField
-            defaultValue={content}
+            defaultValue={text}
             fullWidth
             label="本文"
             margin="normal"
             multiline
-            onBlur={handleContentBlur}
+            onBlur={handleTextBlur}
             variant="outlined"
           />
+
           <ul>
             {messages.map(({ column, index, message, line }) => (
               <li key={index}>{`行${line}, 列${column}: ${message}`}</li>
             ))}
           </ul>
+
+          {navigator.share && (
+            <Box pb={2}>
+              <Button color="primary" onClick={handleShareClick} startIcon={<ShareIcon />} variant="contained">
+                共有
+              </Button>
+            </Box>
+          )}
         </Container>
       </Paper>
 
