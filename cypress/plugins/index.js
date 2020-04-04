b8aa661cd2617227e569173a396ad4825dac932f
Tomoyuki Hata <hato6502@gmail.com>
2020-04-04 22:22:49 +0900

    共有をキャンセルしたときにエラーハンドリングしないようにして

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,6 +1,6 @@
 // / <reference types="cypress" />
 
-module.exports = on => {
+module.exports = (on) => {
   // eslint-disable-next-line global-require,@typescript-eslint/no-var-requires
   on('task', require('@cypress/code-coverage/task'));
 };
