78da3120a83839e8d38915cb2bcb9fafd14591e4
Tomoyuki Hata <hato6502@gmail.com>
2020-04-05 16:56:32 +0900

    textlint-rule-ja-hiragana-hojodoushi を導入して

--- src/dict/initializeDict.ts
+++ src/dict/initializeDict.ts
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import fukushi from './fukushi.yml';
+import hojodoushi from './hojodoushi.yml';
 import kanjiOpen from './kanji-open.yml';
 import spoken from './spoken.yml';
 import typo from './typo.yml';
@@ -7,6 +8,7 @@ import webPlusDb from './web+db.yml';
 
 const initializeDict = () => {
   fs.writeFileSync('fukushi.yml', fukushi);
+  fs.writeFileSync('hojodoushi.yml', hojodoushi);
   fs.writeFileSync('kanji-open.yml', kanjiOpen);
   fs.writeFileSync('spoken.yml', spoken);
   fs.writeFileSync('typo.yml', typo);
