b5d64fed1c54584ec65eaff0f0567f31bc7f180f
Tomoyuki Hata <hato6502@gmail.com>
2020-04-10 00:22:02 +0900

    airbnb の eslint ルールから create-react-app のルールに移行して

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,7 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-  },
   extends: [
-    'airbnb',
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'react-app',
     'plugin:prettier/recommended',
     'plugin:cypress/recommended'
   ],
-  settings: {
-    'import/resolver': {
-      'webpack': {
-        'config': 'webpack.common.js'
-      }
-    }
-  },
-  rules: {
-    '@typescript-eslint/ban-ts-ignore': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    'import/extensions': [
-      'error',
-      {
-        '.js': 'never',
-        '.jsx': 'never',
-        '.ts': 'never',
-        '.tsx': 'never'
-      }
-    ],
-    'react/jsx-filename-extension': [
-      'error',
-      {
-        'extensions': [
-          '.jsx',
-          '.tsx'
-        ]
-      }
-    ],
-    'react/prop-types': 'off',
-  }
 };
