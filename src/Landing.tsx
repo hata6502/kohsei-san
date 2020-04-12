ddabab9f15da1e1813f55bd1d2a6c06898f12a43
Tomoyuki Hata <hato6502@gmail.com>
2020-04-12 23:18:57 +0900

    LP 用に Container を調整して

---
+++ src/Landing.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+import Box from '@material-ui/core/Box';
+
+const TextContainer = styled.div`
+  outline: 0;
+`;
+
+const Landing: React.FunctionComponent = () => <>Landing Page</>;
+
+export default Landing;
