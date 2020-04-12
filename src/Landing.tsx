857b813343cfffee82c1a0af98a6d7bd408dff74
Tomoyuki Hata <hato6502@gmail.com>
2020-04-13 00:16:53 +0900

    ファーストビューを作成して

--- src/Landing.tsx
+++ src/Landing.tsx
@@ -1,11 +1,62 @@
 import React from 'react';
 import styled from 'styled-components';
 import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
 
-const TextContainer = styled.div`
-  outline: 0;
+const FirstView = styled.section`
+  background-image: url('images/publicdomainq-0024864bhwjsw.jpg');
+  background-position: center;
+  background-size: cover;
+  position: relative;
+  text-align: center;
 `;
 
-const Landing: React.FunctionComponent = () => <>Landing Page</>;
+const FirstViewContainer = styled(Container)`
+  ${({ theme }) => `
+    height: ${theme.spacing(60)}px;
+    padding-top: ${theme.spacing(10)}px;
+  `}
+  position: relative;
+`;
+
+const FirstViewWrapper = styled.div`
+  background-color: rgba(255, 255, 255, 0.75);
+  height: 100%;
+  position: absolute;
+  width: 100%;
+`;
+
+const LandingContainer = styled.div`
+  ${({ theme }) => `
+    margin-bottom: ${theme.spacing(2)}px;
+    margin-top: ${theme.spacing(6)}px;
+  `}
+`;
+
+const Landing: React.FunctionComponent = () => (
+  <LandingContainer>
+    <FirstView>
+      <FirstViewWrapper />
+
+      <FirstViewContainer>
+        <Typography component="h1" variant="h3">
+          コンテンツへの信頼度を高めよう
+        </Typography>
+
+        <Box mb={4} mt={8}>
+          <Typography component="h3" variant="subtitle1">
+            その場ですぐに文章を校正できるメモ帳アプリです。
+          </Typography>
+        </Box>
+
+        <Button color="primary" size="large" variant="contained">
+          使ってみる
+        </Button>
+      </FirstViewContainer>
+    </FirstView>
+  </LandingContainer>
+);
 
 export default Landing;
