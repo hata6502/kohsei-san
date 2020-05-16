3858e03d89b575b94cd620333f62a3ab0a19e38f
Tomoyuki Hata <hato6502@gmail.com>
2020-05-16 16:54:16 +0900

    WikiPedia の記事でスコアを測ってみて

--- packages/scripts/src/index.ts
+++ packages/scripts/src/index.ts
@@ -11,7 +11,7 @@ const wikipedia = wiki({
 interface Content {content?: string, title?: string};
 
 const main = async () => {
-  const titles = await wikipedia.random(10);
+  const titles = await wikipedia.random(5);
 
   const queue = new Queue(1);
 
@@ -19,9 +19,10 @@ const main = async () => {
     const page = await wikipedia.page(title);
     const content = (await page.content()) as unknown as Content[];
     const text = content.map(({content, title}) => `${title}\n${content}\n`).join();
+    const result = await lint(text);
+    const score = text.length === 0 ?  0 : result.messages.length / text.length;
 
-    console.log(text);
-    console.log(await lint(text));
+    console.log(score);
   }));
 }
 
